c41aeee3b0a24390f7ffcdfe31d231eb708971d1
Mathieu Lecarme <mathieu@garambrogne.net>
2010-06-27 19:07:18 +0200

    zone detection

--- map2js.py
+++ map2js.py
@@ -6,19 +6,33 @@ __version__ = "0.0.1"
 import Image
 import sys
 
-class Rectangle(object):
-	def __init__(self, color):
-		self.color = color
-		
+class Zone(object):
+	def __init__(self, top, left):
+		self.top = top
+		self.bottom = top
+		self.right= left
+		self.left = left
+	def bottomRight(self, bottom, right):
+		self.bottom = bottom
+		self.right = right
+	def __repr__(self):
+		return "<Zone %i,%i,%i,%i>" % (self.top, self.right, self.bottom, self.left)
 class ImageMap(object):
 	def __init__(self, image):
 		self.image = image
+		self.zones = {}
 	def scan(self):
 		w,h = self.image.size
-		for x in w:
-			for y in h:
-				pixel = self.image.getpixel((x,y))
-				print pixel
+		for x in range(w):
+			for y in range(h):
+				r,g,b,a = self.image.getpixel((x,y))
+				if a > 0:
+					#print r,g,b
+					if (r,g,b) not in self.zones:
+						self.zones[(r,g,b)] = Zone(x, y)
+					self.zones[(r,g,b)].bottomRight(x,y)
 
 if __name__ == '__main__':
-	
+	m = ImageMap(Image.open('map.png'))
+	m.scan()
+	print m.zones
